d65e8aba176c92c48f90884aa7fb7ab2ecace7be
CRISTONI <adonai_cn@hotmail.com>
2022-08-10 21:54:03 -0300

    tests feitos

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { cadastrarCarroDto } from 'src/models/dto/carro.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,32 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('Adiciona um carro ao BD', () => {
+    const testeCarro: cadastrarCarroDto = {
+      modelo: 'TESTE',
+      marca: 'TESTE',
+      anodefabricação: 1999,
+      placa: 'VIX-2020',
+      cor: 'TESTE',
+      chassi: '9BG116GW04C400001',
+      valordecompra: 123,
+    };
+
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/cadastro')
+      .send(testeCarro)
+      .expect(201);
+  });
+
+  it('Mostra todos os carros da agencia', () => {
+    return request(app.getHttpServer()).get('/todos').expect(200);
+  });
+
+  it('Mostra os carros vendidos da agencia', () => {
+    return request(app.getHttpServer()).get('/vendidos').expect(200);
+  });
+
+  it('Mostra os carros disponiveis da agencia', () => {
+    return request(app.getHttpServer()).get('/disponiveis').expect(200);
   });
 });
